657d293ecaea9a8b3b4ec2eb65b6b58e68ae84fc
Alexander Mackenzie <alexmackzie@hotmail.com>
2015-03-17 14:14:02 +0100

    Lagde egen mappe med separat PHP og HTML

---
+++ Kode/php2/html og php separat/connect.php
@@ -0,0 +1,7 @@
+<?php 
+$connect = mysqli_connect("localhost", "root", "Alexander12#", "Reservations");
+// Evaluate the connection 
+if (mysqli_connect_errno()) {
+ echo mysqli_connect_error(); exit(); 
+ } 
+ ?>
